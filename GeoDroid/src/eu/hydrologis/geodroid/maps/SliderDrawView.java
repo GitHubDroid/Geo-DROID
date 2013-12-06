184133b3109c2daf51eef841e15422879b8390ca
GitHubDroid <promptnet@promptnetworks.com>
2013-12-06 11:31:50 -0500

    Use Imperial Units for measurement tool and scale-bar by default rather than metric

--- GeoDroid/src/eu/hydrologis/geodroid/maps/SliderDrawView.java
+++ GeoDroid/src/eu/hydrologis/geodroid/maps/SliderDrawView.java
@@ -74,7 +74,7 @@ public class SliderDrawView extends View {
         super(context, attrs);
 
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-        doImperial = preferences.getBoolean(Constants.PREFS_KEY_IMPERIAL, false);
+        doImperial = preferences.getBoolean(Constants.PREFS_KEY_IMPERIAL, true);
         
         measurePaint.setAntiAlias(true);
         measurePaint.setColor(Color.DKGRAY);
